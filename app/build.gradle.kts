a391907da7bb4a5c32e5614d5e1f5334d822a7a4
marcomorosi.dev <marcomorosi.dev@gmail.com>
2026-03-23 15:51:35 +0100

    Fix: removed APK and converted .gitignore to UTF-8 for F-Droid scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cuscus.wifiaudiostreaming"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.2-beta"
+        versionCode = 6
+        versionName = "0.3.3-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
