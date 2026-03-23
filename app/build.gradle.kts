0260d60150d8aef2dac8d66875175ca0a4766d55
marcomorosi.dev <marcomorosi.dev@gmail.com>
2026-03-23 14:09:05 +0100

    Chore: update gradle wrapper securely and exclude APKs for F-Droid compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cuscus.wifiaudiostreaming"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.1-beta"
+        versionCode = 5
+        versionName = "0.3.2-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
