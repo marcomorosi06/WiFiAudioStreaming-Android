0c0a42f2a333c9413b36de22117281c7cc7040cf
marcomorosi.dev <marcomorosi.dev@gmail.com>
2026-04-03 15:12:05 +0200

    Upgraded version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cuscus.wifiaudiostreaming"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.3-beta"
+        versionCode = 7
+        versionName = "0.4.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
